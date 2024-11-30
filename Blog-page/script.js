7c26c10a88b03a9b5c236693b43848547b52b26b
xtheuchy <elviscayllahuacondori@gmail.com>
2024-11-29 19:36:12 -0500

    1.11

---
+++ Blog-page/script.js
@@ -0,0 +1,14 @@
+new WOW().init();
+
+let options = {
+    strings: ["A Winters Morning"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true,
+    loop: true,
+    showCursor: false,
+};
+
+let typed = new Typed("#title", options);
